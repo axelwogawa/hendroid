7d4d16e21ad05f5ec6544cf2b8a37d8679d01592
axelwogawa <Axel.Wogawa@mailbox.tu-dresden.de>
2020-10-16 22:37:16 +0200

    Replaced mqtt image sender with http sender

---
+++ client-raspi/http_image_sender.py
@@ -0,0 +1,15 @@
+import requests
+
+''' Send images via HTTP
+
+Args:
+    url (str): server address
+    image_path (str): path where png image is stored
+    logger (logging.Logger): logger instance
+'''
+
+def send(url, image_path, logger):
+  with open(image_path, "rb") as image_file:
+    logger.info("Sending image to " + url)
+    x = requests.post(url, data = myobj)
+    logger.info("Reply: " + x.text)
