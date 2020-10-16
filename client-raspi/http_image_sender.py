22613ce35801676c15449fbb6a26393d79d19b77
axelwogawa <Axel.Wogawa@mailbox.tu-dresden.de>
2020-10-16 22:42:26 +0200

    Fixed http image sender copy paste bug

--- client-raspi/http_image_sender.py
+++ client-raspi/http_image_sender.py
@@ -11,5 +11,5 @@ Args:
 def send(url, image_path, logger):
   with open(image_path, "rb") as image_file:
     logger.info("Sending image to " + url)
-    x = requests.post(url, data = myobj)
+    x = requests.post(url, data = image_file)
     logger.info("Reply: " + x.text)
